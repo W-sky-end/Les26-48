6543375641e0b4f9aac7b463df583942a52ce29d
Vadym Kyrylov <aonyko@gmail.com>
2025-10-31 13:47:52 +0200

    Clear database connection credentials
    Removed sensitive database credentials from DB.java.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-    private static final String URL = "jdbc:postgresql://localhost:5432/phonebook";
-    private static final String USER = "wsky";
-    private static final String PASSWORD = "03041988";
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
